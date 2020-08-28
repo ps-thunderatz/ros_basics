a1644d9a9e9b3ebf0f3c68f4d4ce8f2eaa910adf
lucastrschneider <lucastrschneider@gmail.com>
2020-08-28 01:17:58 -0300

    Add two exercises

---
+++ scripts/mimic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Tente implementar o seu próprio mimic, como visto no exemplo. Utilize o rqt para visualizar
+quais tópicos estão sendo publicados e por quem. Da onde você pode pegar as informações que precisa?
+Esse problema tem mais de uma solução possível, pense bem como resolver!
+
+Quando terminar, edite o launch file para que o node executado seja o seu próprio. E lembre de
+atualizar o CMakeLists!!
+"""
+
+# Quais outros pacotes precisam ser importados?
+import rospy
+
+# Quais funções auxiliares serão necessárias?
+
+if __name__ == '__main__':
+    try:
+        pass # Comece por aqui!
+    except rospy.ROSInterruptException:
+        pass
---
+++ scripts/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Tente implementar um node que controle a sua tartaruga para que ela desenha um quadrado na tela.
+
+Quando terminar, crie um launch file que execute o seu node e a simulação. E lembre de
+atualizar o CMakeLists!!
+"""
+
+# Quais outros pacotes precisam ser importados?
+import rospy
+
+# Quais funções auxiliares serão necessárias?
+
+if __name__ == '__main__':
+    try:
+        pass # Comece por aqui!
+    except rospy.ROSInterruptException:
+        pass
