50786f4e30ce1a5b7332f1190293026f6e0076f8
felipeGdM <felipegmelo.42@gmail.com>
2020-08-31 11:54:22 -0300

    :art: Drop "%" operator

--- scripts/listener.py
+++ scripts/listener.py
@@ -3,8 +3,8 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
+    rospy.loginfo(f'{rospy.get_caller_id()} I heard {data.data}')
+
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
